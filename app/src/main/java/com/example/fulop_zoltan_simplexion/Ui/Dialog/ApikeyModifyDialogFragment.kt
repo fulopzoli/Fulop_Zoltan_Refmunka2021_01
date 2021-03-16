50befd563bd2b39616a72b628b8131ba46bb2f20
zoli <fulop_zoli@hotmail.hu>
2021-03-16 10:43:46 +0100

    Debugging

--- app/src/main/java/com/example/fulop_zoltan_simplexion/Ui/Dialog/ApikeyModifyDialogFragment.kt
+++ app/src/main/java/com/example/fulop_zoltan_simplexion/Ui/Dialog/ApikeyModifyDialogFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.fulop_zoltan_simplexion.Other.Utility
 import com.example.fulop_zoltan_simplexion.R
